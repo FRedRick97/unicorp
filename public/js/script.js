a9252988139e0d4da8f17f7ddc7facd12f8281a6
FRedRick97 <chopra.aakash7@gmail.com>
2018-06-15 20:35:48 +0530

    Front Completed with mobile first

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,36 @@
 $(function () {
-    $('#email').blur(error);
+    // $('.board').setTimeOut(function () {
+    //     $(this).slideUp();
+    // }, 1000);
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 100) {
+            $('nav').addClass('bg-dark');
+        } else {
+            $('nav').removeClass('bg-dark');
+        }
+    });
+    var check = function () {
+        if ($(window).width() >= 992) {
+            $('body').css("background-color", "blue");
+            $('ul.navbar-nav').children().removeClass('bg-white');
 
-    var error = function (name) {
-        $(this)
-            .css('box-shadow', '1px 1px 5px red')
-            .css('border', '1px solid red');
-        console.log('Hello ' + name);
-    }
+            $('.board').removeClass('small').addClass('big');
+        }
+
+        if ($(window).width() < 992) {
+            $('body').css("background-color", "lightgreen");
+            $('ul.navbar-nav').children().removeClass('bg-white');
+
+            $('.board').removeClass('big').addClass('small');
+        }
+
+        if ($(window).width() <= 768) {
+            $('ul.navbar-nav').children().addClass('bg-white');
+        }
+    };
+    check();
+    $(window).resize(check);
+
 
 
 
