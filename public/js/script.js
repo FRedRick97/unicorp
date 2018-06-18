5001f1b517f6c1badf778101f8884d1ceaacc259
FRedRick97 <chopra.aakash7@gmail.com>
2018-06-19 00:09:13 +0530

    added smooth scrooling + active links

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,57 @@
 $(function () {
     $(window).scroll(function () {
         if ($(document).scrollTop() > 100) {
-            $('nav').addClass('bg-dark');
+            $('nav').css('transition', 'background-color 0.4s linear').addClass('bg-dark');
         } else {
             $('nav').removeClass('bg-dark');
         }
     });
     var check = function () {
         if ($(window).width() >= 992) {
-            $('body').css("background-color", "blue");
-            // $('ul.navbar-nav').children().removeClass('bg-white');
-
-            // $('.board').removeClass('small').addClass('big');
+            $('body').css("background-color", "rgb(170,57,57)");
         }
 
         if ($(window).width() < 992) {
             $('body').css("background-color", "lightgreen");
-            // $('ul.navbar-nav').children().removeClass('bg-white');
-
-            // $('.board').removeClass('big').addClass('small');
         }
 
-        if ($(window).width() <= 768) {
-            // $('ul.navbar-nav').children().addClass('bg-white');
-        }
     };
     check();
     $(window).resize(check);
 
-    $('li.nav-item').on('click', function () {
-        // var x = $('a', this);
-        // $(this).find('a').not(x).removeClass('active');
-        // $(x).addClass('active');
+    var scrollLink = $('.scroll');
+    scrollLink.on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            $(this).parent().siblings().removeClass('active');
+            $('body, html').animate({
+                scrollTop: $(this.hash).offset().top - $('nav').height()
+            }, 1000);
+        }
+
+    });
+
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+        var navHeight = $('nav').height();
+        var navLink = $('ul.navbar-nav li');
+        $('section').each(function () {
+            var sectionOffset = $(this).offset().top;
+            var targetHref = '#' + $(this).attr('id');
+            // console.log(targetHref);
+            if (sectionOffset <= (scrollbarLocation + navHeight)) {
+                navLink.find(function () {
+                    var a = $('a', this);
+                    a.each(function () {
+                        // console.log($(this).attr('href'));
+                        if ($(this).attr('href') === targetHref) {
+                            $(this).parent().addClass('active');
+                            $(this).parent().siblings().removeClass('active');
+
+                        }
+                    });
+                });
+            }
+        });
     });
 });
