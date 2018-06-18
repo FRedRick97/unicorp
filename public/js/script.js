a69c3efb84c86c9af763d2be23da6a6cbc1ea394
FRedRick97 <chopra.aakash7@gmail.com>
2018-06-18 13:15:55 +0530

    transitions added

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,4 @@
 $(function () {
-    $('button').on("click", function () {
-        $(this).css('border', 'none');
-    });
     $(window).scroll(function () {
         if ($(document).scrollTop() > 100) {
             $('nav').addClass('bg-dark');
@@ -12,22 +9,28 @@ $(function () {
     var check = function () {
         if ($(window).width() >= 992) {
             $('body').css("background-color", "blue");
-            $('ul.navbar-nav').children().removeClass('bg-white');
+            // $('ul.navbar-nav').children().removeClass('bg-white');
 
             // $('.board').removeClass('small').addClass('big');
         }
 
         if ($(window).width() < 992) {
             $('body').css("background-color", "lightgreen");
-            $('ul.navbar-nav').children().removeClass('bg-white');
+            // $('ul.navbar-nav').children().removeClass('bg-white');
 
             // $('.board').removeClass('big').addClass('small');
         }
 
         if ($(window).width() <= 768) {
-            $('ul.navbar-nav').children().addClass('bg-white');
+            // $('ul.navbar-nav').children().addClass('bg-white');
         }
     };
     check();
     $(window).resize(check);
+
+    $('li.nav-item').on('click', function () {
+        // var x = $('a', this);
+        // $(this).find('a').not(x).removeClass('active');
+        // $(x).addClass('active');
+    });
 });
