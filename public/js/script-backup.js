5f42d463fb07eb007721d0e15775ca3e2c80bd4e
FRedRick97 <chopra.aakash7@gmail.com>
2018-06-19 15:47:32 +0530

    upper complete --left hover active

--- public/js/script-backup.js
+++ public/js/script-backup.js
@@ -5,55 +5,85 @@ $(function () {
         } else {
             $('nav').removeClass('bg-dark');
         }
-    });
-    var check = function () {
-        if ($(window).width() >= 992) {
-            $('body').css("background-color", "rgb(170,57,57)");
-            // $('ul.navbar-nav').children().removeClass('bg-white');
 
-            // $('.board').removeClass('small').addClass('big');
-        }
-
-        if ($(window).width() < 992) {
-            $('body').css("background-color", "lightgreen");
-            // $('ul.navbar-nav').children().removeClass('bg-white');
-
-            // $('.board').removeClass('big').addClass('small');
-        }
-
-        if ($(window).width() <= 768) {
-            // $('ul.navbar-nav').children().addClass('bg-white');
-        }
-    };
-    check();
-    $(window).resize(check);
-
-    var scrollLink = $('.scroll');
-    scrollLink.on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            $(this).parent().siblings().removeClass('active');
-            $('body, html').animate({
-                scrollTop: $(this.hash).offset().top - $('nav').height()
-            }, 1000);
-        }
-
-    });
-
-    $(window).scroll(function () {
         var scrollbarLocation = $(this).scrollTop();
         var navHeight = $('nav').height();
-        scrollLink.each(function () {
-            var hash = $(this.hash);
-            var sectionOffset = $(hash).offset().top;
+        var scrollPos = $(document).scrollTop() + 100;
 
-            if (sectionOffset <= (scrollbarLocation + navHeight)) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-                // if (hash !== undefined) {
+        var scroll = function () {
+            $('.scroll').each(function () {
+                var currLink = $(this);
+                var refElement = $(currLink.attr("href"));
+                // console.log(refElement);
 
-                // }
-            }
-        });
+                if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                    currLink.parent().siblings().removeClass("active");
+                    currLink.parent().addClass("active");
+                }
+            });
+        };
+
+        if (scrollbarLocation + $(window).height() == $(document).height()) {
+            var last = $('.scroll').parent(':last');
+            // $('.scroll').parent(':last').find('a').attr('href')
+            last.addClass('active');
+            last.siblings().removeClass('active');
+            // $('.scroll').parent().siblings().removeClass("active");
+            // $('.scroll').parent().addClass("active");
+        } else {
+            scroll();
+        }
     });
+
 });
+var check = function () {
+    if ($(window).width() >= 992) {
+        $('body').css("background-color", "rgb(170,57,57)");
+        // $('ul.navbar-nav').children().removeClass('bg-white');
+
+        // $('.board').removeClass('small').addClass('big');
+    }
+
+    if ($(window).width() < 992) {
+        $('body').css("background-color", "lightgreen");
+        // $('ul.navbar-nav').children().removeClass('bg-white');
+
+        // $('.board').removeClass('big').addClass('small');
+    }
+
+    if ($(window).width() <= 768) {
+        // $('ul.navbar-nav').children().addClass('bg-white');
+    }
+};
+check();
+$(window).resize(check);
+
+var scrollLink = $('.scroll');
+scrollLink.on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        // $(this).parent().siblings().removeClass('active');
+        $('body, html').animate({
+            scrollTop: $(this.hash).offset().top - $('nav').height()
+        }, 1000);
+    }
+
+});
+
+    // $(window).scroll(function () {
+    // var scrollbarLocation = $(this).scrollTop();
+    // var navHeight = $('nav').height();
+    //     scrollLink.each(function () {
+    //         var hash = $(this.hash);
+    //         var sectionOffset = $(hash).offset().top;
+
+    //         if (sectionOffset <= (scrollbarLocation + navHeight)) {
+    //             $(this).parent().addClass('active');
+    //             $(this).parent().siblings().removeClass('active');
+    //             // if (hash !== undefined) {
+
+    //             // }
+    //         }
+    //     });
+    // });
+// });
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,47 @@
 $(function () {
     $(window).scroll(function () {
         if ($(document).scrollTop() > 100) {
-            $('nav').css('transition', 'background-color 0.4s linear').addClass('bg-dark');
+            $('a.navbar-brand, .nav-link').removeClass('white-color').addClass('black-color');
+            $('nav').css('transition', 'background-color 0.4s linear').addClass('bg-white');
         } else {
-            $('nav').removeClass('bg-dark');
+            $('nav').removeClass('bg-white');
+            $('a.navbar-brand, .nav-link').removeClass('black-color').addClass('white-color');
         }
+
+        var scrollbarLocation = $(this).scrollTop();
+        var navHeight = $('nav').height();
+        var navLink = $('ul.navbar-nav li');
+        var first = $('section:first');
+
+        $('section').each(function () {
+            var sectionOffset = $(this).offset().top;
+            var targetHref = '#' + $(this).attr('id');
+
+            if ((scrollbarLocation + navHeight) < first.offset().top) {
+                $('li.nav-item').removeClass('active').addClass('black-color');
+            }
+
+
+            if (sectionOffset <= (scrollbarLocation + navHeight)) {
+                navLink.find(function () {
+                    var a = $('a', this);
+                    a.each(function () {
+                        if ($(this).attr('href') === targetHref) {
+                            $(this).parent().removeClass('black-color').addClass('active');
+                            $(this).parent().siblings().removeClass('active').addClass('black-color');
+                        }
+                    });
+                });
+            }
+        });
     });
     var check = function () {
         if ($(window).width() >= 992) {
             $('body').css("background-color", "rgb(170,57,57)");
         }
-
         if ($(window).width() < 992) {
             $('body').css("background-color", "lightgreen");
         }
-
     };
     check();
     $(window).resize(check);
@@ -23,35 +50,19 @@ $(function () {
     scrollLink.on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
-            $(this).parent().siblings().removeClass('active');
-            $('body, html').animate({
+            $('body, html').stop().animate({
                 scrollTop: $(this.hash).offset().top - $('nav').height()
             }, 1000);
         }
+    });
+
+    var topscroll = $('.top-scroll');
+    topscroll.on('click', function (event) {
+        event.preventDefault();
+        $('body, html').stop().animate({
+            scrollTop: 0
+        }, 1000);
 
     });
 
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
-        var navHeight = $('nav').height();
-        var navLink = $('ul.navbar-nav li');
-        $('section').each(function () {
-            var sectionOffset = $(this).offset().top;
-            var targetHref = '#' + $(this).attr('id');
-            // console.log(targetHref);
-            if (sectionOffset <= (scrollbarLocation + navHeight)) {
-                navLink.find(function () {
-                    var a = $('a', this);
-                    a.each(function () {
-                        // console.log($(this).attr('href'));
-                        if ($(this).attr('href') === targetHref) {
-                            $(this).parent().addClass('active');
-                            $(this).parent().siblings().removeClass('active');
-
-                        }
-                    });
-                });
-            }
-        });
-    });
 });
